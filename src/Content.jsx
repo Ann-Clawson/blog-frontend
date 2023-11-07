7186c748a7b92b42dffbaf821ff2da017da1ddae
Ann Clawson <annmasonclawson@gmail.com>
2023-11-07 11:42:57 -0800

    installs axios and imports in app.jsx

--- src/Content.jsx
+++ src/Content.jsx
@@ -14,21 +14,7 @@ export function Content() {
     setIsPostsShowVisible(false);
   };
 
-  let posts = [
-    {
-      id: 1,
-      title: "Pizza",
-      body: "I like food, food tastes good.",
-      image:
-        "https://images.squarespace-cdn.com/content/v1/58a1ba5503596e1e0cb09ac3/1689714027921-364C907ROU4Z1QGKZONZ/230522_PrimePizza8034.jpg?format=1500w",
-    },
-    {
-      id: 2,
-      title: "Orange Cats",
-      body: "They are just the best.",
-      image: "https://i.redd.it/zeh71flzvzz61.jpg",
-    },
-  ];
+  let posts = [];
 
   return (
     <div>
