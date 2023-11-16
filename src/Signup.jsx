a4d88595c1a244243146be52f9b243a8a4c83381
Ann Clawson <annmasonclawson@gmail.com>
2023-11-16 09:54:28 -0800

    adds handlesubmit function in signup

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,7 +1,23 @@
+import axios from "axios";
+
 export function Signup() {
+  const handleSubmit = (event) => {
+    event.preventDefault;
+    const params = new FormData(event.target);
+    console.log("handle submit", params);
+    axios
+      .post("http://localhost:3000/users.json", params)
+      .then((response) => {
+        console.log(response.data);
+        event.target.reset();
+      })
+      .catch((error) => {
+        console.log(event.repsonse.data.errors);
+      });
+  };
   return (
     <div id="signup">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           Name: <input name="name" type="text" />
         </div>
