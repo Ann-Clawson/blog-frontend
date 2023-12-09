4d28a0b53030139c7f2d7f8fc354fe97c7049124
Ann Clawson <annmasonclawson@gmail.com>
2023-12-09 15:32:11 -0800

    centers footer, still not at bottom

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,9 @@
 export function Footer() {
   return (
-    <footer id="footer" className="container position-absolute start-50 translate-middle">
-      <p>&copy; cat lady 2023</p>
+    <footer>
+      <div id="footer" className="container">
+        <p>&copy; cat lady 2023</p>
+      </div>
     </footer>
   );
 }
