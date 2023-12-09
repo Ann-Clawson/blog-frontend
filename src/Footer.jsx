dee6d28cf1c69c7005cb4121842b9bfe2d554fb4
Ann Clawson <annmasonclawson@gmail.com>
2023-12-09 15:07:38 -0800

    lowers, centers, and pads footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer id="footer" className="container">
+    <footer id="footer" className="container position-absolute start-50 translate-middle">
       <p>&copy; cat lady 2023</p>
     </footer>
   );
