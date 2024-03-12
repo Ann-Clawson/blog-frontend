3f1714d50c50b46992a69f205c7d756ddfa46539
Ann Clawson <annmasonclawson@gmail.com>
2024-03-12 11:52:20 -0700

    props read in home component

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,6 @@
-export function Home() {
+export function Home(props) {
+  let post = props.posts[0];
+  console.log(post);
   return (
     <>
       <div className="container" id="home">
@@ -7,6 +9,9 @@ export function Home() {
           <i>It&apos;s a Blog!</i>
           <br /> where every day it&apos;s a blog.
         </h1>
+        <h2>{post.title}</h2>
+        {/* <p>{props.posts.body}</p>
+        <img src={props.posts.image} alt={props.posts.title} /> */}
       </div>
     </>
   );
