38907675db7909aaf24a0cb85061186c6f6a3c39
Ann Clawson <annmasonclawson@gmail.com>
2024-04-01 12:33:32 -0700

    makes home page posts index

--- src/Content.jsx
+++ src/Content.jsx
@@ -85,8 +85,8 @@ export function Content() {
         <Route path="/login" element={<Login />} />
         <Route path="/logout" element={<Logout />} />
         <Route path="/posts/new" element={<PostsNew onCreatePost={handleCreatePost} />} />
-        <Route path="/posts" element={<PostsIndex posts={posts} onShowPost={handleShowPost} />} />
-        <Route path="/home" element={<Home posts={posts} />} />
+        <Route path="/home" element={<PostsIndex posts={posts} onShowPost={handleShowPost} />} />
+        {/* <Route path="/home" element={<Home posts={posts} />} /> */}
       </Routes>
       <Modal show={isPostsShowVisible} onClose={handleClose}>
         <PostsShow onUpdatePost={handleUpdatePost} post={currentPost} onDestroyPost={handleDestroyPost} />
