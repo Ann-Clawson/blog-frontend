2047fdc5274f401d4c4cecfc00700c160e4a13d6
Ann Clawson <annmasonclawson@gmail.com>
2024-04-19 13:48:59 -0700

    housekeeping

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,6 @@ import { BrowserRouter } from "react-router-dom";
 // import { useState, useEffect } from "react";
 
 function App() {
-  // const [flashMessage, setFlashMessage] = useState("");
-
-  // const getFlashMessageFromLocalStorage = () => {
-  //   if (localStorage.flashMessage) {
-  //     setFlashMessage(localStorage.flashMessage);
-  //     localStorage.removeItem("flashMessage");
-  //   }
-  // };
-
-  // useEffect(getFlashMessageFromLocalStorage, []);
-
   return (
     <div>
       <BrowserRouter>
