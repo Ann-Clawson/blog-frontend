021a822ec3e0e1f28a8f8cdfca34b57513aa0730
Ann Clawson <annmasonclawson@gmail.com>
2024-04-19 13:49:08 -0700

    housekeeping

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Content } from "./Content";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
 import { BrowserRouter } from "react-router-dom";
-// import { useState, useEffect } from "react";
 
 function App() {
   return (
